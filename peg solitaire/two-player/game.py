67854f9c48af91e0749d3039cb33236d26cdf5fb
samuelturner1606 <46319151+samuelturner1606@users.noreply.github.com>
2022-02-16 12:22:33 +0000

    basic render

--- peg solitaire/two-player/game.py
+++ peg solitaire/two-player/game.py
@@ -1,10 +1,29 @@
 from dataclasses import dataclass
+import mailbox
 
 @dataclass 
 class State():
     '''Representation of the board game in binary.'''
-    bitboard1: int = 0
-    bitboard2: int = 0
+    bitboard1: int = 161107674112
+    '''
+    0000000
+    0000100
+    1011000
+    0010110
+    0010000
+    0001000
+    0000000
+    '''
+    bitboard2: int = 106103477157904
+    '''
+    0011000
+    0010000
+    0000010
+    0100000
+    1000011
+    0000000
+    0010000
+    '''
 
     def possible_moves(self) -> int:
         '''Find all possible next moves given the state.'''
@@ -13,7 +32,12 @@ class State():
     def mailbox(self) -> list[int]:
         '''Convert the state into mailbox representation.'''
         # (int & 1 << n and 1) gets the n'th bit of an int
-        return [ (self.bitboard1 & (1 << n) and 1) + 2*(self.bitboard2 & (1 << n) and 1) for n in range(BYTE_LENGTH-1,-1,-1) ]
+        return [ (self.bitboard1 & (1 << n) and 1) + 2*(self.bitboard2 & (1 << n) and 1) for n in range(BYTE_LENGTH) ]
+    
+    def make2D(self) -> list[list]:
+        '''Remove bitboard row seperator bits and split it by the number of rows.'''
+        mailbox = self.mailbox()
+        return [ mailbox[ HEIGHT*row : HEIGHT*row+HEIGHT ] for row in range(WIDTH) ]
 
 class Solitaire2():
     '''A two-player peg solitaire game class.'''
@@ -35,6 +59,9 @@ class Solitaire2():
     
     def render(self) -> None:
         '''Processes and prints the current state to the terminal.'''
+        twoD = self.state.make2D()
+        for r in twoD:
+            print(r)
 
     def is_game_over(self):
         '''Checks whether any terminal states have been reached.'''
@@ -47,9 +74,20 @@ class Solitaire2():
     
 def main():
     game = Solitaire2()
+    game.render()
 
 if __name__ == "__main__":
     WIDTH, HEIGHT = 7, 7
     BYTE_LENGTH = WIDTH*HEIGHT
     FULL_BITMASK = 2**(BYTE_LENGTH) - 1 # used to remove extra bits from a left bitshift
+    BOARD_BIMASK = 124141734710812 # English cross board shape
+    '''
+    0011100
+    0011100
+    1111111
+    1111111
+    1111111
+    0011100
+    0011100
+    '''
     main()
