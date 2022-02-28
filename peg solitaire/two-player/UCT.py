3ee533409044ac30e53abbf788724dff57bce1ec
samuelturner1606 <46319151+samuelturner1606@users.noreply.github.com>
2022-02-28 17:41:41 +0000

    reorganising

--- peg solitaire/two-player/UCT.py
+++ peg solitaire/two-player/UCT.py
@@ -8,99 +8,26 @@ BOARD_BITMASK = 0b00011100000111000111111101111111011111110001110000011100
 DEFAULT_BOARD1 = 0b00001100000001000001001100110110011001000001000000011000
 DEFAULT_BOARD2 = 0b10000000110000110110001000001000110110000110000000100
 
-class UCT:
-    'Monte Carlo tree search using UCT algorithm. First rollout the tree then choose a move.'
-    def __init__(self, C=1):
-        self.Q = defaultdict(int)  # total reward of each node
-        self.N = defaultdict(int)  # total visit count of each node
-        self.tree = dict()  # game tree of node states
-        self.C = C # exploration weight
-
-    def choose(self, node):
-        'Choose the best successor of node. (Choose a move in the game)'
-        if node.is_terminal():
-            raise RuntimeError(f'choose called on terminal node {node}')
-        if node not in self.tree:
-            return node.find_random_child()
-
-        def score(n):
-            if self.N[n] == 0:
-                return float('-inf')  # avoid unseen moves
-            return self.Q[n] / self.N[n]  # average reward
-
-        return max(self.tree[node], key=score)
-
-    def do_rollout(self, node):
-        'Make the tree one layer better. (Train for one iteration.)'
-        path = self._tree_policy(node)
-        leaf = path[-1]
-        self._expand(leaf)
-        reward = self._default_policy(leaf)
-        self._backup(path, reward)
-
-    def _tree_policy(self, node):
-        'Find an unexplored descendent of `node`'
-        path = []
-        while True:
-            path.append(node)
-            if node not in self.tree or not self.tree[node]:
-                # node is either unexplored or terminal
-                return path
-            unexplored = self.tree[node] - self.tree.keys()
-            if unexplored:
-                n = unexplored.pop()
-                path.append(n)
-                return path
-            node = self._best_child(node)  # descend a layer deeper
-
-    def _expand(self, node):
-        'Update the `tree` dict with the tree of `node`'
-        if node in self.tree:
-            return  # already expanded
-        self.tree[node] = node.find_tree()
-
-    def _default_policy(self, node):
-        'Returns the reward for a random simulation (to completion) of `node`'
-        invert_reward = True
-        while True:
-            if node.is_terminal():
-                reward = node.reward()
-                return 1 - reward if invert_reward else reward
-            node = node.find_random_child()
-            invert_reward = not invert_reward
-
-    def _backup(self, path, reward):
-        'Send the reward back up to the ancestors of the leaf.'
-        for node in reversed(path):
-            self.N[node] += 1
-            self.Q[node] += reward
-            reward = 1 - reward  # 1 for me is 0 for my enemy, and vice versa
-
-    def _best_child(self, node):
-        'Select a child of node, balancing exploration & exploitation.'
-
-        # All tree of node should already be expanded:
-        assert all(n in self.tree for n in self.tree[node])
-
-        log_N_vertex = math.log(self.N[node])
-
-        def uct(n):
-            'Upper confidence bound for trees'
-            return self.Q[n] / self.N[n] + self.C * math.sqrt(
-                log_N_vertex / self.N[n]
-            )
-
-        return max(self.tree[node], key=uct)
-
-
 class Node():
-    '''A bitboard representation of a two-player peg solitaire board state.'''
+    'A bitboard representation of a two-player peg solitaire board state.'
     def __init__(self, bitboard1: int, bitboard2: int, current_player: bool) -> None:
         self.bitboards = [bitboard2, bitboard1]
         self.player = current_player # player 1 = True, player 2 = False
 
+    def find_children(self) -> set:
+        'All possible successors of this board state.'
+        successors = []
+        for p in [self.player, not self.player]:
+            for d in DIRECTIONS:
+                legal_ends = self._legal_move_ends(self.bitboards[p], d)
+                if legal_ends:
+                    pass
+            if successors:
+                break
+        return set(successors)
+
     def find_random_child(self):
-        'Random successor node of this board state.'
+        'Random successor `node` of this board state.'
         directions = DIRECTIONS.copy()
         while directions:
             shuffle(directions)
@@ -109,46 +36,9 @@ class Node():
             if legal_ends:
                 split_ends = self._split(legal_ends)
                 random_end = choice(split_ends)
-                end_to_start = self._smudge(random_end, -random_direction)
-                bitboard1 = self.bitboards[1] & ~end_to_start
-                bitboard2 = self.bitboards[0] & ~end_to_start
-                if self.player == 1:
-                    bitboard1 |= random_end
-                else:
-                    bitboard2 |= random_end
-                return Node(bitboard1, bitboard2, not self.player)
+                return self._successor(random_end, random_direction)
         return Node(self.bitboards[1], self.bitboards[0], not self.player) # if current player has no moves, turn is skipped
     
-    @staticmethod
-    def _smudge(binary: int, direction: int) -> int:
-        '''Dilate twice the on-bits of a `binary` number in a `direction`.'''
-        if direction < 0:
-            direction = -direction
-            return binary | (binary >> direction) | (binary >> 2*direction)
-        else:
-            return binary | (binary << direction) | (binary << 2*direction)
-    
-    @staticmethod
-    def _bitshift(binary: int, offset: int) -> int:
-        '''Does right or left bitshift depending on sign of `offset`.'''
-        if offset < 0:
-            offset = -offset
-            return binary >> offset
-        else:
-            return binary << offset
-
-    @staticmethod
-    def _split(legal_moves: int) -> list[int]:
-        '''Split `legal moves` into invidual moves by the on-bits.'''
-        seperate_moves = []
-        n = 0
-        while legal_moves:
-            if legal_moves & 1: # get least sig. bit
-                seperate_moves.append(1 << n)
-            n += 1
-            legal_moves >>= 1 # remove least sig. bit
-        return seperate_moves
-
     def _legal_move_ends(self, bitboard: int, direction: int) -> int:
         '''Get the ending squares of all legal moves from a players' `bitboard` in a `direction`. 
         The on-bits in the returned integer represent where a players' pegs can move to.'''
@@ -174,6 +64,43 @@ class Node():
         else:
             return 1 # player 1 wins
 
+    def _successor(self, move_end, direction): 
+        'Create a successor `node` given the ending square of the move and the `direction` it was moved.'
+        # Dilate twice the on-bits of a binary number in a direction
+        if direction < 0:
+            direction = -direction
+            end_to_start = move_end | (move_end << direction) | (move_end << 2*direction)
+        else:
+            end_to_start = move_end | (move_end >> direction) | (move_end >> 2*direction)
+        bitboard1 = self.bitboards[1] & ~end_to_start
+        bitboard2 = self.bitboards[0] & ~end_to_start
+        if self.player == 1:
+            bitboard1 |= move_end
+        else:
+            bitboard2 |= move_end
+        return Node(bitboard1, bitboard2, not self.player)
+    
+    @staticmethod
+    def _bitshift(binary: int, offset: int) -> int:
+        'Does right or left bitshift depending on sign of `offset`.'
+        if offset < 0:
+            offset = -offset
+            return binary >> offset
+        else:
+            return binary << offset
+
+    @staticmethod
+    def _split(legal_moves: int) -> list[int]:
+        'Split `legal moves` into invidual moves by the on-bits.'
+        seperate_moves = []
+        n = 0
+        while legal_moves:
+            if legal_moves & 1: # get least sig. bit
+                seperate_moves.append(1 << n)
+            n += 1
+            legal_moves >>= 1 # remove least sig. bit
+        return seperate_moves
+
     def __hash__(self) -> int:
         return self.bitboards[0] | self.bitboards[1]
 
@@ -184,7 +111,7 @@ class Node():
             return NotImplemented
 
 def render(bitboard1: int = 0, bitboard2: int = 0, message: str = '') -> None:
-        '''Print the bitboards in colour.'''
+        'Print the bitboards in colour.'
         for row in range(ROWS):
             output_row = ''
             for n in range(COLS*row,COLS*row+COLS):
@@ -210,12 +137,77 @@ def render(bitboard1: int = 0, bitboard2: int = 0, message: str = '') -> None:
         print(f'{message}\n')
         return
 
-n = Node(0b1000000010000010000001000, 0b100000000000, 1)
-render(n.bitboards[1],n.bitboards[0])
-n = n.find_random_child()
-render(n.bitboards[1],n.bitboards[0])
-n = n.find_random_child()
-render(n.bitboards[1],n.bitboards[0])
-t = n.is_terminal()
-r = n.reward()
-pass
+class UCT:
+    'Monte Carlo tree search using UCT algorithm. First rollout the tree then choose a move.'
+    def __init__(self, C=1):
+        self.Q = defaultdict(int)  # total reward of each node
+        self.N = defaultdict(int)  # total visit count of each node
+        self.tree = dict()  # game tree of node states
+        self.C = C # exploration weight
+
+    def choose(self, node: Node):
+        'Choose the best successor of node. (Choose a move in the game)'
+        if node.is_terminal():
+            raise RuntimeError(f'choose called on terminal node {node}')
+        if node not in self.tree:
+            return node.find_random_child()
+
+        def score(n):
+            if self.N[n] == 0:
+                return float('-inf')  # avoid unseen moves
+            return self.Q[n] / self.N[n]  # average reward
+
+        return max(self.tree[node], key=score)
+
+    def do_rollout(self, node: Node):
+        'Make the tree one layer better. (Train for one iteration.)'
+        path = self._tree_policy(node)
+        leaf = path[-1]
+        self._expand(leaf)
+        reward = self._default_policy(leaf)
+        self._backup(path, reward)
+
+    def _tree_policy(self, node: Node):
+        'Find an unexplored descendent of `node`'
+        path = []
+        while True:
+            path.append(node)
+            if node not in self.tree or not self.tree[node]:
+                # node is either unexplored or terminal
+                return path
+            unexplored = self.tree[node] - self.tree.keys()
+            if unexplored:
+                n = unexplored.pop()
+                path.append(n)
+                return path
+            node = self._best_child(node)  # descend a layer deeper
+
+    def _expand(self, node: Node):
+        'Update the `tree` dict with the tree of `node`'
+        if node in self.tree:
+            return  # already expanded
+        self.tree[node] = node.find_children()
+
+    def _default_policy(self, node: Node):
+        'Returns the reward for a random simulation (to completion) of `node`'
+        invert_reward = True
+        while True:
+            if node.is_terminal():
+                reward = node.reward()
+                return 1 - reward if invert_reward else reward
+            node = node.find_random_child()
+            invert_reward = not invert_reward
+
+    def _backup(self, path, reward):
+        'Send the reward back up to the ancestors of the leaf.'
+        for node in reversed(path):
+            self.N[node] += 1
+            self.Q[node] += reward
+            reward = 1 - reward  # 1 for me is 0 for my enemy, and vice versa
+
+    def _best_child(self, node: Node):
+        '''Select the best child `node`, balancing exploration & exploitation. 
+        All children should already be expanded.'''
+        assert all(n in self.tree for n in self.tree[node])
+        log_N_vertex = math.log(self.N[node])
+        return max(self.tree[node], key = lambda n: self.Q[n] / self.N[n] + self.C * math.sqrt(log_N_vertex / self.N[n]))
